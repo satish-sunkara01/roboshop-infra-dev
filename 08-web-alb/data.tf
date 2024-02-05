abe01b9744533836c5424fc11b111528e255816e
satish-sunkara <satishsunkara01@gmail.com>
2024-02-05 21:20:57 +0530

    rob web.app-dev commit

--- 08-web-alb/data.tf
+++ 08-web-alb/data.tf
@@ -5,3 +5,7 @@ data "aws_ssm_parameter" "web_alb_sg_id" {
 data "aws_ssm_parameter" "public_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/public_subnet_ids"
 }
+
+data "aws_ssm_parameter" "acm_certificate_arn" {
+  name = "/${var.project_name}/${var.environment}/acm_certificate_arn"
+}
--- 08-web-alb/main.tf
+++ 08-web-alb/main.tf
@@ -17,13 +17,15 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.web_alb.arn
   port              = "443"
   protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = data.aws_ssm_parameter.acm_certificate_arn.value
 
   default_action {
     type = "fixed-response"
 
     fixed_response {
       content_type = "text/plain"
-      message_body = "Hi, This response is from APP ALB"
+      message_body = "Hi, This response is from web- ALB"
       status_code  = "200"
     }
   }
