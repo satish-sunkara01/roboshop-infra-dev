6eb8cc21abddb1025d72a50c26a5a27a1ac063e4
satish-sunkara <satishsunkara01@gmail.com>
2024-02-07 19:03:30 +0530

    whole project roboshop

---
+++ 12-shipping/variables.tf
@@ -0,0 +1,27 @@
+variable "common_tags" {
+  default = {
+    Project     = "roboshop"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+variable "tags" {
+  default = {
+    Component = "shipping"
+  }
+}
+
+variable "project_name" {
+  default = "roboshop"
+}
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_name" {
+  default = "saachi.online"
+}
+variable "iam_instance_profile" {
+  default = "Instanace_creation"
+}
